0817fd26882845d3d8413f5e8bf26afc2c1b3bfd
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 00:47:49 +0530

    Implement circular queue using array

---
+++ Circular Queue (Array).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define SIZE 5
+
+int queue[SIZE];
+int front = -1, rear = -1;
+
+void enqueue(int x) {
+    if ((rear + 1) % SIZE == front) {
+        printf("Full\n");
+        return;
+    }
+    if (front == -1) front = 0;
+    rear = (rear + 1) % SIZE;
+    queue[rear] = x;
+}
+
+int dequeue() {
+    if (front == -1) return -1;
+
+    int val = queue[front];
+
+    if (front == rear)
+        front = rear = -1;
+    else
+        front = (front + 1) % SIZE;
+
+    return val;
+}
