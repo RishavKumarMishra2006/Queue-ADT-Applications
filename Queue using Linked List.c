25f9b017e4b2be86a7d70bcf921fe406a81ab784
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 00:47:04 +0530

    Add queue implementation using linked list
    Implemented a queue using a linked list with enqueue and dequeue functions.

---
+++ Queue using Linked List.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = temp;
+        return;
+    }
+    rear->next = temp;
+    rear = temp;
+}
+
+int dequeue() {
+    if (front == NULL) return -1;
+
+    struct Node* temp = front;
+    int val = temp->data;
+    front = front->next;
+
+    if (front == NULL) rear = NULL;
+
+    free(temp);
+    return val;
+}
+
+int main() {
+    enqueue(5);
+    enqueue(15);
+    printf("%d\n", dequeue());
+    return 0;
+}
