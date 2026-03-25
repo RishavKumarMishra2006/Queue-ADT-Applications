fd3541ee8199543413637e7ff3c1e7fa96d26909
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 01:01:33 +0530

    Add MyQueue class using two stacks
    Implement a queue using two stacks with push, pop, and peek methods.

---
+++ Hackerrank Queue using Two Stacks.py
@@ -0,0 +1,19 @@
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def pop(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        self.s2.pop()
+
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
